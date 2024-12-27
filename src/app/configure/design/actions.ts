443c6f6651315f0be798c84da68db715cc1d9457
Natnaelkete <ketemanatnael023@gmail.com>
2024-12-27 14:05:13 -0800

    add design, auth-callback, preview, stripe payment and more functionality

---
+++ src/app/configure/design/actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { db } from "@/db";
+import {
+  CaseColor,
+  CaseFinish,
+  CaseMaterial,
+  PhoneModels,
+} from "@prisma/client";
+
+export type SaveConfigArgs = {
+  color: CaseColor;
+  finish: CaseFinish;
+  material: CaseMaterial;
+  model: PhoneModels;
+  configId: string;
+};
+
+export async function saveConfig({
+  color,
+  finish,
+  material,
+  model,
+  configId,
+}: SaveConfigArgs) {
+  await db.configuration.update({
+    where: { id: configId },
+    data: { color, finish, material, model },
+  });
+}
